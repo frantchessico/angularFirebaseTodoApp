6f816d46982f20b3aae61cc80426724aba396a79
Francisco  Inoque <40237204+frantchessico@users.noreply.github.com>
2019-12-14 14:57:41 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDrZH3Oou9-p3jNomEG6Dgjm2ExONgQang',
-    authDomain: 'naminhaloja.firebaseapp.com',
-    databaseURL: 'https://naminhaloja.firebaseio.com',
-    projectId: 'naminhaloja',
-    storageBucket: 'naminhaloja.appspot.com',
-    messagingSenderId: '663817652034',
-    appId: '1:663817652034:web:1f5c86eeddb3aa96'
+    apiKey: "#",
+    authDomain: "#",
+    databaseURL: "#",
+    projectId: "#",
+    storageBucket: "#",
+    messagingSenderId: "#",
+    appId: "#"
   }
 };
 
